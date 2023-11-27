ddb208a638d621edf842f88175bf134857b02431
Robert Fiske <r.fiske@csuohio.edu>
2023-11-27 18:40:56 -0500

    conflict 1 change

--- src.c
+++ src.c
@@ -4,7 +4,7 @@ int main()
 {
 	printf("adding intro\n");
 	printf("Hello world\n");
-	printf("this is a print from dev 1\n");
+	printf("this is a print from dev 1 deniedx\n");
 
 	printf("Now Exiting progapplication\n");
 
