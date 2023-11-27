5b61b46b237194173359d19c7440bd164af811e3
Robert Fiske <r.fiske@csuohio.edu>
2023-11-27 18:40:25 -0500

    conflict from 2

--- src.c
+++ src.c
@@ -4,9 +4,9 @@ int main()
 {
 	printf("adding intro\n");
 	printf("Hello world\n");
-	printf("this is a print from dev 1\n");
+	printf("this is a print from dev 1 denied\n");
 
-	printf("this is a commit from dev 2\n");
+	printf("this is a commit from dev 2 approved\n");
 	printf("Now Exiting progapplication\n");
 
 	return(0);
